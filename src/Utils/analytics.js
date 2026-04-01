cf17b37dc73984d46d8b3659105faf537ab25806
Anuj ID <anuj@inquisitivedigital.com>
2026-04-01 11:00:00 +0530

    analytics

--- src/Utils/analytics.js
+++ src/Utils/analytics.js
@@ -56,16 +56,16 @@ class AnalyticsWrapper {
     if (this.scriptsLoaded) return Promise.resolve();
 
     return new Promise((resolve) => {
-      const gtmIdVal = import.meta.env.VITE_GTM_ID || "GTM-K6TCGLZS";
+      const gtmIdVal = import.meta.env.VITE_GTM_ID || "GTM-WMZR9JNW";
       const pixelIdVal = import.meta.env.VITE_PIXEL_ID || "4021930454788333";
 
       // ── Google Tag Manager <script> ──────────────────────────
       const gtmScript = document.createElement("script");
       gtmScript.innerHTML = `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','${gtmIdVal}');
       `;
       document.head.appendChild(gtmScript);
